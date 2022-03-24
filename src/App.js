280d5050f77a7590c0a632049c903d91ec8a74c4
AvetBadalyan <avetbadalyan@gmail.com>
2022-03-24 21:43:31 +0400

    feat: add components and connect to each other

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header/Header";
+import React, { useState } from "react";
+import TodoList from "./TodoList/TodoList";
 
 function App() {
+  let data = [
+    {
+      id: 1,
+      task: "task 1",
+      complete: true,
+    },
+    {
+      id: 2,
+      task: "task 2",
+      complete: false,
+    },
+  ];
+
+  const [toDoList, setToDoList] = useState(data);
+
   return (
     <div className="App">
-      
+      <Header />
+      <TodoList />
     </div>
   );
 }
---
+++ src/TodoList/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from '../Todo/Todo'
+
+export default function TodoList() {
+  return (
+      <div>
+          <Todo />
+          
+    </div>
+  )
+}
